010ed7663f805483caa066285b205178a054ca2d
David Rose-Franklin <99210748+DavidRR-F@users.noreply.github.com>
2024-05-06 16:52:21 -0400

    Update database.py

--- {{cookiecutter.project_slug}}/script/managers/database.py
+++ {{cookiecutter.project_slug}}/script/managers/database.py
@@ -1,7 +1,8 @@
 from ..core import env
 
+from typing import Optional, List, Type, Union
 import pandas as pd
-from sqlalchemy import create_engine, text
+from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 
 
@@ -10,27 +11,40 @@ class DatabaseManager:
         self.engine = create_engine(database_url)
         self.session = sessionmaker(bind=self.engine)
 
-    def execute(self, sql, **params):
+    def _execute_query(self, sql, select=None, **params):
         with self.session() as session:
-            with session.begin():
-                result = session.execute(text(sql), params)
-            return result
+            result = session.execute(sql, params)
+            if select == "FIRST":
+                return result.first()
+            if select == "ALL":
+                return result.fetchall()
+            return None
 
-    def select_all(self, sql, cls=None, **params):
-        result = self.execute_query(sql, **params)
-        if cls:
-            return [cls(row) for row in result.fetchall()]
-        return result.fetchall()
+    def execute(self, sql, **params):
+        self._execute_query(sql, **params)
 
-    def select_first(self, sql, clas=None, **params):
-        result = self.execute_query(sql, **params)
+    def select_all(
+        self, sql, cls: Optional[Type] = None, **params
+    ) -> Union[List[object], List[tuple], List[None]]:
+        results = self._execute_query(sql, select="ALL", **params)
         if cls:
-            return cls(result.fetchone())
-        return result.fetchone()
+            return [cls(*result) for result in results]
+        return results
+
+    def select_first(
+        self, sql, cls: Optional[Type] = None, **params
+    ) -> Union[object, tuple, None]:
+        result = self._execute_query(sql, select="FIRST", **params)
+        if cls:
+            return cls(*result) if result else None
+        return result
 
     def select_dataframe(self, sql, **params) -> pd.DataFrame:
         with self.engine.connect() as conn:
             return pd.read_sql(sql, con=conn, params=params)
+
+
+dm = DatabaseManager(env.db_uri)
 
 
 dm = DatabaseManager(env.db_uri)

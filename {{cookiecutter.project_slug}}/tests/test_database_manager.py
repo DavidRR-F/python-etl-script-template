38221d07d701309a307018b04e39870ed5f8daae
David Rose-Franklin <99210748+DavidRR-F@users.noreply.github.com>
2024-05-20 16:33:31 -0400

    Update test_database_manager.py

--- {{cookiecutter.project_slug}}/tests/test_database_manager.py
+++ {{cookiecutter.project_slug}}/tests/test_database_manager.py
@@ -1,5 +1,5 @@
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import text
 
 from script.managers.database import DatabaseManager
 
@@ -8,38 +8,40 @@ from script.managers.database import DatabaseManager
 def db_manager():
     DATABASE_URL = "sqlite:///:memory:"
     manager = DatabaseManager(DATABASE_URL)
-    manager.execute_query(
-        "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)"
+    manager.execute(
+        text(
+            "CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, name TEXT, email TEXT)"
+        )
     )
     yield manager
-    manager.execute_query("DROP TABLE users")
+    manager.execute(text("DROP TABLE users"))
 
 
 def test_insert(db_manager):
-    db_manager.insert(
-        "INSERT INTO users (name, email) VALUES (:name, :email)",
+    db_manager.execute(
+        text("INSERT INTO users (name, email) VALUES (:name, :email)"),
         name="test",
         email="test",
     )
-    assert db_manager.select_first("SELECT * FROM users") == (1, "test", "test")
+    assert db_manager.select_first(text("SELECT * FROM users")) == (1, "test", "test")
 
 
 def test_select_first(db_manager):
-    db_manager.insert(
-        "INSERT INTO users (name, email) VALUES (:name, :email)",
+    db_manager.execute(
+        text("INSERT INTO users (name, email) VALUES (:name, :email)"),
         name="test2",
         email="test2",
     )
-    assert db_manager.select_first("SELECT * FROM users") == (1, "test", "test")
+    assert db_manager.select_first(text("SELECT * FROM users")) == (1, "test", "test")
 
 
 def test_select_all(db_manager):
-    assert db_manager.select_all("SELECT * FROM users") == [
+    assert db_manager.select_all(text("SELECT * FROM users")) == [
         (1, "test", "test"),
         (2, "test2", "test2"),
     ]
 
 
 def test_select_dataframe(db_manager):
-    df = db_manager.select_dataframe("SELECT * FROM users")
+    df = db_manager.select_dataframe(text("SELECT * FROM users"))
     assert df.shape == (2, 3)
